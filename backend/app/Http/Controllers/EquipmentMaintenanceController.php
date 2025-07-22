c87c54fff2c623ad7434b9b80d3b478f4d6c351b
That-VEN <thattven@gmail.com>
2025-07-22 10:24:37 +0700

    make migrate

---
+++ backend/app/Http/Controllers/EquipmentMaintenanceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment_Maintenance;
+use Illuminate\Http\Request;
+
+class EquipmentMaintenanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Equipment_Maintenance $equipment_Maintenance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Equipment_Maintenance $equipment_Maintenance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Equipment_Maintenance $equipment_Maintenance)
+    {
+        //
+    }
+}
---
+++ backend/app/Http/Controllers/MaterialPurchaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material_Purchase;
+use Illuminate\Http\Request;
+
+class MaterialPurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Material_Purchase $material_Purchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Material_Purchase $material_Purchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Material_Purchase $material_Purchase)
+    {
+        //
+    }
+}
