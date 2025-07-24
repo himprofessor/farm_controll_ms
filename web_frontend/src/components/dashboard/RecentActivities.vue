d08adceccee29fbf46f8f12e04a5d98de42cca71
ya-chhoeun <ya.chhoeun@student.passerellesnumeriques.org>
2025-07-24 14:35:53 +0700

    add icon

--- web_frontend/src/components/dashboard/RecentActivities.vue
+++ web_frontend/src/components/dashboard/RecentActivities.vue
@@ -7,7 +7,12 @@
       <div class="space-y-4">
         <div v-for="activity in activities" :key="activity.id" class="flex items-start space-x-3">
           <div :class="activity.iconClass" class="flex-shrink-0 w-8 h-8 rounded-full flex items-center justify-center">
-            <component :is="activity.icon" class="w-4 h-4 text-white" />
+            <svg v-if="activity.icon === 'check'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-white">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+            <svg v-else-if="activity.icon === 'warning'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-white">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z" />
+            </svg>
           </div>
           <div class="flex-1 min-w-0">
             <p class="text-sm text-gray-900">{{ activity.message }}</p>
@@ -29,39 +34,31 @@ export default {
           id: 1,
           message: 'New staff member John Doe added',
           time: '2 hours ago',
-          icon: 'CheckIcon',
+          icon: 'check',
           iconClass: 'bg-green-500'
         },
         {
           id: 2,
           message: 'Feed stock running low (5 days remaining)',
           time: '4 hours ago',
-          icon: 'WarningIcon',
+          icon: 'warning',
           iconClass: 'bg-yellow-500'
         },
         {
           id: 3,
           message: 'Salary payment processed for 24 staff',
           time: '1 day ago',
-          icon: 'CheckIcon',
+          icon: 'check',
           iconClass: 'bg-green-500'
         },
         {
           id: 4,
           message: 'Medication batch expires in 7 days',
           time: '2 days ago',
-          icon: 'WarningIcon',
+          icon: 'warning',
           iconClass: 'bg-yellow-500'
         }
       ]
-    }
-  },
-  components: {
-    CheckIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>`
-    },
-    WarningIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z"></path></svg>`
     }
   }
 }
--- web_frontend/src/components/dashboard/StatsCard.vue
+++ web_frontend/src/components/dashboard/StatsCard.vue
@@ -7,7 +7,22 @@
         <p class="text-sm text-gray-500 mt-1">{{ change }}</p>
       </div>
       <div :class="iconClasses" class="p-3 rounded-lg">
-        <component :is="iconComponent" class="w-6 h-6 text-white" />
+        <!-- Render the appropriate SVG based on icon prop -->
+        <svg v-if="icon === 'users'" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+        </svg>
+        <svg v-else-if="icon === 'dollar'" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"></path>
+        </svg>
+        <svg v-else-if="icon === 'box'" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
+        </svg>
+        <svg v-else-if="icon === 'trending'" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
+        </svg>
+        <svg v-else class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
+        </svg>
       </div>
     </div>
   </div>
@@ -32,29 +47,6 @@ export default {
         purple: 'bg-purple-500'
       }
       return colors[this.color] || 'bg-gray-500'
-    },
-    iconComponent() {
-      const icons = {
-        users: 'UsersIcon',
-        dollar: 'DollarIcon',
-        box: 'BoxIcon',
-        trending: 'TrendingIcon'
-      }
-      return icons[this.icon] || 'BoxIcon'
-    }
-  },
-  components: {
-    UsersIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>`
-    },
-    DollarIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"></path></svg>`
-    },
-    BoxIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>`
-    },
-    TrendingIcon: {
-      template: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>`
     }
   }
 }
