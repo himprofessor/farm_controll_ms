b6661f4fd95fbd04410ae1f818378e3ec5ff3aa3
Phally-Chheang <phally.chheang@student.passerellesnumeriques.org>
2025-07-25 15:44:48 +0700

    Update router/index.js

--- web_frontend/src/router/index.js
+++ web_frontend/src/router/index.js
@@ -1,11 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import DashboardView from '../views/Dashboard.vue'  // Correct path based on your structure
-
 
 // Use consistent aliasing
 import HomeScreen from '@/components/HomeScreen.vue';
-// import Login from '@/components/Login.vue'; // use @ instead of relative path
-// import StaffManagement from '@/views/StaffManagement.vue';
 
 const routes = [
   {
@@ -13,16 +9,7 @@ const routes = [
     name: 'Home',
     component: HomeScreen
   },
-  // {
-  //   path: '/login',
-  //   name: 'Login',
-  //   component: Login
-  // },
-  // {
-  //   path: '/Staff',
-  //   name: 'StaffManagement',
-  //   component: StaffManagement
-  // }
+ 
 ];
 
 const router = createRouter({
