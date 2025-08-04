a9bef6a5f0308c4dd6bfcc781efba1a80139ad49
That-VEN <thattven@gmail.com>
2025-08-04 09:09:41 +0700

    check

--- web_frontend/src/plugin/axios.js
+++ web_frontend/src/plugin/axios.js
@@ -10,7 +10,6 @@ const apiClient = axios.create({
   withCredentials: false
 });
 
-<<<<<<< HEAD
 export default apiClient;
 export const setAuthToken = (token) => {
   if (token) {
@@ -22,6 +21,3 @@ export const setAuthToken = (token) => {
 export const clearAuthToken = () => {
     delete apiClient.defaults.headers.common['Authorization'];
 }
-=======
-export default API
->>>>>>> development
